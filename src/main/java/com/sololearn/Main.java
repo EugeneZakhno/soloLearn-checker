2d8dd39b2395e4666b2a5c462309ea5700dfc58d
EugeneZakhno <zohan1234567@gmail.com>
2020-03-19 23:27:05 +0300

    added class Main

--- src/main/java/com/sololearn/Main.java
+++ src/main/java/com/sololearn/Main.java
@@ -3,7 +3,9 @@ package com.sololearn;
 public class Main {
     public static void main(String[] args) {
 
-      //  What is the output of this code ?
+         //  What is the output of this code ?
+        // https://photos.app.goo.gl/gcuZMnYjdQYDEQ63A
+
                 String y = "12333dfghj@gmail.com";
         if (y.substring(y.length() - 5).equals(".com")) //always false
             System.out.print(y.substring(1));
