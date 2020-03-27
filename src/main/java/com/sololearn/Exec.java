85f3628b7237fd9eba8536e0f5820c4f01ee2c34
EugeneZakhno <zohan1234567@gmail.com>
2020-03-27 20:16:11 +0300

    added class Exec

---
+++ src/main/java/com/sololearn/Exec.java
@@ -0,0 +1,16 @@
+package com.sololearn;
+
+// https://photos.app.goo.gl/M48VCkAo3RuRRe9j8
+class Exec{ // class cannot be static
+        public static int alter(Integer y)
+        {
+            y=y*10;
+            return y;
+        }
+
+    public static void main(String [] argS) {
+        Integer x = new Integer(3);
+        System.out.print(x +  Exec.alter(x));
+    }
+
+    }
