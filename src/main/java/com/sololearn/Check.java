fd242730ed3363d0bdcf27b6a6a19a60ede2abf3
EugeneZakhno <zohan1234567@gmail.com>
2020-03-29 21:50:21 +0300

    added class Check

--- src/main/java/com/sololearn/Check.java
+++ src/main/java/com/sololearn/Check.java
@@ -1,12 +1,12 @@
 package com.sololearn;
 
 
-
+//https://photos.app.goo.gl/jTmfqqJF8frXt2TZ7
         interface P {
             void print(int x);
         }
         class Check implements P{
-            void print(int x){
+             void print(int x){  // print needs to be public
                 System.out.print(x);
             }
             public static void main(String...arg){
