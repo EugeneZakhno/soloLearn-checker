a60cc4127b1aee9abd8133855df9a82f63d637ec
EugeneZakhno <zohan1234567@gmail.com>
2020-04-01 21:34:40 +0300

    added class Programm

---
+++ src/main/java/com/sololearn/Programm.java
@@ -0,0 +1,23 @@
+package com.sololearn;
+
+public class Programm {
+
+    public static class A {
+        public String a1;
+
+        public A(String a1) {
+            this.a1 = a1;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        A a = new A("1");
+        A c = a;
+        a = new A("3");
+        System.out.print(a.a1);
+        System.out.print(c.a1);
+
+    }
+
+}
