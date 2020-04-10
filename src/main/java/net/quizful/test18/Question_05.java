654586dcf8393c3747bb84507085adf358ccade2
EugeneZakhno <zohan1234567@gmail.com>
2020-04-10 09:44:55 +0300

    added Test18 Question_04

---
+++ src/main/java/net/quizful/test18/Question_05.java
@@ -0,0 +1,21 @@
+package net.quizful.test18;
+
+public class Question_05 {
+}
+class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2};
+        int x;
+      /*
+
+          for (x : array) {
+            System.out.println(x);
+        }
+
+        */
+    }
+    // Ошибка компиляции
+  //  Пояснение: При использовании for-each переменная, использующаяся для хранения значения из массива или коллекции,
+    //  должна быть объявлена внутри цикла, иначе получим ошибку компиляции.
+
+}
