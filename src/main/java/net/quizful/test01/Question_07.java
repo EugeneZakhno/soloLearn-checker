91711abf6412a54ef937aa3803577f965ec77f65
EugeneZakhno <zohan1234567@gmail.com>
2020-04-10 08:34:31 +0300

    added Test18 Question_01

--- src/main/java/net/quizful/test01/Question_07.java
+++ src/main/java/net/quizful/test01/Question_07.java
@@ -1,4 +1,27 @@
 package net.quizful.test01;
 
 public class Question_07 {
+   public static  void  main (String[] str){
+       String A = "Test2";
+       A += "55";
+       System.out.println(A);
+
+     //  String A = "Test1";
+     //   A -= "23";
+
+     //  Integer int2 = new Integer (15);
+     //  int2;
+
+       Integer int1 = new Integer (41);
+       int1 -= 33;
+       System.out.println(int1);
+
+       // Пояснение: В отличие от C/C++, в Java в качестве операторов может использоваться
+       // ограниченное количество арифметических выражений: присваивание, инкремент/декремент (++/--),
+       // вызов метода, создание объекта. Всё остальное недопустимо.
+       // Например, "Math.abs(1);" - корректно, а "Math.abs(1) + Math.abs(2);" - уже ошибка.
+   }
+
+
+
 }
---
+++ src/main/java/net/quizful/test18/Question_01.java
@@ -0,0 +1,10 @@
+package net.quizful.test18;
+
+public class Question_01 {
+    public static void main(String[] args) {
+        Double d1 = 1d;
+        Double d2 = 1d;
+        System.out.println(d1 == d2);
+    }
+}
+// output: false
