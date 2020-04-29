e3e98c9e7cf754406d7186cc6fa0dcc379edb60f
EugeneZakhno <zohan1234567@gmail.com>
2020-04-29 11:27:31 +0300

    added HowItWorks check values

--- src/main/java/io/github/myownCheck/HowItWorks.java
+++ src/main/java/io/github/myownCheck/HowItWorks.java
@@ -2,9 +2,24 @@ package io.github.myownCheck;
 
 public class HowItWorks {
     public static void main(String[] args) {
-        char ch = '\uD83D';
 
-        System.out.println(ch);
+        double d1 = 1d;
+        double d2 = 1d;
+
+        Double dd1 = 1d;
+        Double dd2 = 1d;
+
+        Integer i1 = 10;
+        Integer i2 = 10;
+
+        String s1 = "re";
+        String s2 = "re";
+
+        System.out.println(d1==d2);
+        System.out.println(dd1==dd2);
+        System.out.println(i1==i2);
+        System.out.println(i2==10);
+        System.out.println(s1==s2);
+
     }
-
 }
